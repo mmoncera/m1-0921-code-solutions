e3d5f21706e138147ef100acb7ce9fa0331afdb7
Michael Moncera <michaelmoncera@gmail.com>
2021-10-28 20:06:20 -0700

    Completed array-manipulation-1 exercise.

---
+++ array-manipulation-1/src/head.js
@@ -0,0 +1,7 @@
+/* exported head */
+function head(array) {
+  return array[0];
+}
+
+// define a function named head which takes a sigle paramater of 'array'
+// return the paramater 'array' at index 0
---
+++ array-manipulation-1/src/last.js
@@ -0,0 +1,7 @@
+/* exported last */
+function last(array) {
+  return array[array.length - 1];
+}
+
+// define a function named last which takes a sigle paramater of 'array'
+// return the paramater 'array' at the last index (result of subracting 1 from the length of 'array')
