2bd67fd1d3cc03bec1b9b4e66fbdc7688de49101
Michael Moncera <michaelmoncera@gmail.com>
2021-10-29 00:16:17 -0700

    Completed array-manipulation-2 exercise.

---
+++ array-manipulation-2/src/drop.js
@@ -0,0 +1,7 @@
+/* exported drop */
+function drop(array, count) {
+  return array.slice(count);
+}
+
+// define a function named capitalizeWord which takes a sigle paramater of 'word'
+// return the parameter 'array' starting from the index of the 'count' parameter
