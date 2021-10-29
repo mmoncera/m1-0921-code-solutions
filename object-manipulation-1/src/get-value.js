c6cbbb094f253804b99a732938bcf7fb580026c4
Michael Moncera <michaelmoncera@gmail.com>
2021-10-28 20:47:46 -0700

    Completed object-manipulation-1 exercise.

---
+++ object-manipulation-1/src/get-value.js
@@ -0,0 +1,7 @@
+/* exported getValue */
+function getValue(object, key) {
+  return object[key];
+}
+
+// define a function named getValue which takes two paramaters of 'object' and 'key' respectively
+// return the value from the parameters 'object' at 'key'
---
+++ object-manipulation-1/src/set-value.js
@@ -0,0 +1,7 @@
+/* exported setValue */
+function setValue(object, key, value) {
+  object[key] = value;
+}
+
+// define a function named setValue which takes 3 paramaters of 'object', 'key', and 'value' respectively
+// assign the parameter 'value' to the parameters 'object' at 'key'
