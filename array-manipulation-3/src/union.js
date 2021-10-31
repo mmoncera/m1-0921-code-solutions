bdb784948ed6fda97172e8fed9f06bc0fff2e0e4
Michael Moncera <michaelmoncera@gmail.com>
2021-10-31 13:22:02 -0700

    Refactored union.js.

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,12 +1,4 @@
 /* exported union */
 function union(first, second) {
-  var result = [...first];
-
-  for (let i = 0; i < second.length; i++) {
-    if (!result.includes(second[i])) {
-      result.push(second[i]);
-    }
-  }
-
-  return result;
+  return [...new Set([...first, ...second])];
 }
