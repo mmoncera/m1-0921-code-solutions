18d500c06b0b4e957e040e53486fdbfe9361675a
Michael Moncera <michaelmoncera@gmail.com>
2021-11-04 19:10:25 -0700

    Completed exercise dom-querying.

---
+++ dom-querying/main.js
@@ -0,0 +1,19 @@
+console.log('Hello World');
+
+var h1 = document.querySelector('h1');
+
+console.log('value of h1:', h1);
+console.dir(h1);
+
+var idExplanation = document.querySelector('#explanation');
+
+console.log('value of idExplanation:', idExplanation);
+console.dir(idExplanation);
+
+var allP = document.querySelectorAll('p');
+
+console.log('value of allP:', allP);
+
+var allClassExampleLink = document.querySelectorAll('.example-link');
+
+console.log('value of allClassExampleLink:', allClassExampleLink);
