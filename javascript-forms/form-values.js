efd6106ad1b8c853ca33dff2f1ca21b4b8ce8b51
Michael Moncera <michaelmoncera@gmail.com>
2021-11-15 20:18:00 -0800

    created values object

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -3,11 +3,13 @@ var $forms = document.querySelector('#contact-form');
 $forms.addEventListener('submit', function (event) {
   event.preventDefault();
 
-  console.log('messageData:', {
+  var values = {
     name: $forms[0].value,
     email: $forms[1].value,
     message: $forms[2].value
-  });
+  };
+
+  console.log('messageData:', values);
 
   $forms.reset();
 });
